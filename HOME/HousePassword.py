6c02b181158aca573e31b610fb24879dd8f7621e
Administrator <93186976@qq.com>
2018-03-12 21:19:40 +0800

    first

--- HOME/HousePassword.py
+++ HOME/HousePassword.py
@@ -3,5 +3,18 @@ __author__ = '朱永刚'
 
 import re
 
-def HousePassword():
-    pass
+def checkio0(data):
+    if len(data) < 10:
+        return False
+    if not re.compile('[A-Z]+').findall(data):
+        return False
+    if not re.compile('[a-z]+').findall(data):
+        return False
+    if not re.compile('[0-9]+').findall(data):
+        return False
+    return True
+
+def checkio(data):
+    return True if re.search("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$", data) and len(data) >= 10 else False
+
+
--- HOME/__init__.py
+++ HOME/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding:utf-8 -*-
 __author__ = '朱永刚'
+
+"""
+地址https://py.checkio.org/mission/house-password/
+
+问题：密码长度大于等于十个，至少有一个数字，一个大写字母和一个小写字母
+"""
